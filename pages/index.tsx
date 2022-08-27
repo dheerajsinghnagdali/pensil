a6df8df0c0c5eeedaa80f398b9dd0ed72e2148a7
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2022-08-27 23:16:28 +0530

    fix(pages/index.tsx): image in team section

--- pages/index.tsx
+++ pages/index.tsx
@@ -511,7 +511,7 @@ const Home: NextPage = () => {
               <img
                 src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
                 alt="Max portrait"
-                className="aspect-square object-cover object-top [@media(min-width:336px)]:w-80"
+                className="aspect-square w-full object-cover object-top [@media(min-width:336px)]:w-80"
               />
             </div>
           </div>
